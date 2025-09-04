8e0e7a36e6c0e7dfb99bd3e767242d87dba13336
luiz.fmsilva23 <luiz.fmsilva23@TIT0676436W11-1>
2025-09-04 16:43:19 -0300

    Adicionar arquivos de projeto.

---
+++ HelloMiau/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloMiau
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HelloMiau/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HelloMiau
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
